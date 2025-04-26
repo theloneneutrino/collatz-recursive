9240b27772d734bf2c917279c80bd411f80eda25
Luke Warkentin <theloneneutrino@gmail.com>
2025-04-25 18:38:16 -0700

    making the integer reader

--- src/main.c
+++ src/main.c
@@ -1,8 +1,26 @@
 #include <stdio.h>
 
 int charToInt (char* thisguy) {
-  fprintf(stdout,"Your input was %s.\n",thisguy);
-  return -1;
+  unsigned int result = 0;
+  unsigned int it = 0;
+
+  while (thisguy[it] != 0) {
+    if (('0' <= thisguy[it]) && (thisguy[it] <= '9')) {
+      result += thisguy[it] - 0x30;
+    }
+    else {
+      fprintf(stderr,"Error: your input was not a positive integer.");
+    }
+
+
+    if (thisguy[it + 1] != 0)
+      result *= 10;
+
+    it++;
+  }
+
+  fprintf(stdout,"Your input was %i.\n",result);
+  return result;
 }
 
 int main(int argc, char** argv)
@@ -10,10 +28,11 @@ int main(int argc, char** argv)
   switch (argc)
     {
     case 2:
-      charToInt(argv[0]);
+      charToInt(argv[1]);
       break;
     default:
       fprintf(stderr,"Error: Invalid usage of %s.\n", argv[0]);
+      fprintf(stdout,"Usage: %s <number>\nWhere number is a positive integer.", argv[0]);
       return 1;
     }
 }
